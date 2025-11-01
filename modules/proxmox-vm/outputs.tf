4fb12318c32d79590e71238dcbc1a699f3618b60
Sebastian Freund <abes140377@web.de>
2025-11-01 18:50:24 +0100

    fix: correct ipv4 output to use actual network interface IP

--- modules/proxmox-vm/outputs.tf
+++ modules/proxmox-vm/outputs.tf
@@ -1,4 +1,4 @@
 output "ipv4" {
   description = "The IPv4 address of the VM."
-  value       = try(values(proxmox_virtual_environment_vm.this.ipv4_addresses)[0], null)
+  value       = try(proxmox_virtual_environment_vm.this.ipv4_addresses[1][0], null)
 }
