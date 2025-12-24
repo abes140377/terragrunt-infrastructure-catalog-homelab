6f47f752c90974cce7af40db4c2421515fa9982a
Sebastian Freund <abes140377@web.de>
2025-12-24 07:59:09 +0100

    fix: update homelab provider version constraint to >= 0.2.0 and refactor naming modules

--- examples/tofu/naming/main.tf
+++ examples/tofu/naming/main.tf
@@ -1,14 +1,10 @@
 terraform {
   required_providers {
     homelab = {
-      source = "registry.terraform.io/abes140377/homelab"
+      source  = "registry.terraform.io/abes140377/homelab"
+      version = ">= 0.2.0"
     }
   }
-}
-
-variable "env" {
-  description = "Environment name"
-  type        = string
 }
 
 variable "app" {
@@ -16,14 +12,26 @@ variable "app" {
   type        = string
 }
 
-module "naming" {
+module "naming_dev" {
   source = "../../../modules/naming"
 
-  env = var.env
+  env = "dev"
   app = var.app
 }
 
-output "name" {
-  description = "Generated name"
-  value       = module.naming.generated_name
+module "naming_prod" {
+  source = "../../../modules/naming"
+
+  env = "prod"
+  app = var.app
+}
+
+output "name_dev" {
+  description = "Generated dev name"
+  value       = module.naming_dev.generated_name
+}
+
+output "name_prod" {
+  description = "Generated prod name"
+  value       = module.naming_prod.generated_name
 }
